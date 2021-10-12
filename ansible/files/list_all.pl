b988582cad4cadad53efbc103a4489ead2d96b27
joealcazar <44735368+joealcazar@users.noreply.github.com>
2021-10-12 11:52:37 -0500

    Update list_all.pl

--- ansible/files/list_all.pl
+++ ansible/files/list_all.pl
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE list_all (employee_id NUMBER) AS
+CREATE OR REPLACE PROCEDURE tec_list_all (employee_id NUMBER) AS
    tot_emps NUMBER;
    BEGIN
       SELECT * FROM Test;
