0770f77c33504a04001df3342138923757f6e6d3
joealcazar <44735368+joealcazar@users.noreply.github.com>
2021-10-12 11:52:16 -0500

    Update add_row.pl

--- ansible/files/add_row.pl
+++ ansible/files/add_row.pl
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE add_row (f_name VARCHAR, l_name VARCHAR) AS
+CREATE OR REPLACE PROCEDURE tec_add_row (f_name VARCHAR, l_name VARCHAR) AS
    tot_emps NUMBER;
    BEGIN
       INSERT INTO Test (first_name, last_name) VALUES ('JOE', 'BLACK');
